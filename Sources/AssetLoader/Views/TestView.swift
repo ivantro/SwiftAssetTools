de492669b7d950479223e312d8d5dcfac072de1b
Ivan Trotter <ivantrotter@gmail.com>
2025-08-15 18:14:50 -0400

    Added testing UI View

---
+++ Sources/AssetLoader/Views/TestView.swift
@@ -0,0 +1,14 @@
+//
+//  TestView.swift
+//  AssetLoader
+//
+//  Created by Ivan Trotter on 8/15/25.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Ohh yeahh!, the package works!")
+    }
+}

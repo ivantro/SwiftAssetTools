d2022870f0f50d13728b9c74bbbcda71755bf04e
Ivan Trotter <ivantrotter@gmail.com>
2025-08-16 13:34:41 -0400

    Added public initializer

--- Sources/AssetLoader/Views/SplashScreenAppixView.swift
+++ Sources/AssetLoader/Views/SplashScreenAppixView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 public struct SplashScreenAppixView: View {
+    
+    public init() {}
+    
     public var body: some View {
         Image("splash1242x2208", bundle: .module)
             .resizable()

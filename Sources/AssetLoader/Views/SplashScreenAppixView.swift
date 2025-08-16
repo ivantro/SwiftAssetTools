50deec87b0a0ebf90a4c48d0d384bbe6329b5ab9
Ivan Trotter <ivantrotter@gmail.com>
2025-08-16 13:33:06 -0400

    Added vertical SplashScreen view

---
+++ Sources/AssetLoader/Views/SplashScreenAppixView.swift
@@ -0,0 +1,16 @@
+//
+//  SplashScreenAppixView.swift
+//  AssetLoader
+//
+//  Created by Ivan Trotter on 8/16/25.
+//
+
+import SwiftUI
+
+public struct SplashScreenAppixView: View {
+    public var body: some View {
+        Image("splash1242x2208", bundle: .module)
+            .resizable()
+            .scaledToFit()
+    }
+}

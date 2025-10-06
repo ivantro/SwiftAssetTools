0283a9d742875bea09c3c66cc76e6202b945ad4b
Ivan Trotter <ivantrotter@gmail.com>
2025-10-06 11:10:44 -0400

    Added a progress bar to splashscreen

--- Sources/AssetLoader/Views/SplashScreenAppixView.swift
+++ Sources/AssetLoader/Views/SplashScreenAppixView.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 public struct SplashScreenAppixView: View {
+    /// Optional progress current value to render a progress bar.
+    private let progressCurrent: Int?
+    /// Optional total value for the progress bar.
+    private let progressTotal: Int?
     
-    public init() {}
+    public init(progressCurrent: Int? = nil, progressTotal: Int? = nil) {
+        self.progressCurrent = progressCurrent
+        self.progressTotal = progressTotal
+    }
     
     public var body: some View {
-        Image("splash1242x2208", bundle: .module)
-            .resizable()
-            .scaledToFit()
+        ZStack(alignment: .bottom) {
+            Image("splash1242x2208", bundle: .module)
+                .resizable()
+                .scaledToFit()
+            
+            if let current = progressCurrent, let total = progressTotal, total > 0 {
+                VStack {
+                    ProgressView(value: Double(current), total: Double(total))
+                        .progressViewStyle(.linear)
+                        .tint(.white)
+                        .padding(.horizontal, 24)
+                        .padding(.bottom, 24)
+                }
+            }
+        }
     }
 }
